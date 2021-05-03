31db21b55853ea5954408f864cc8861d0ea203df
vsirisakd <victor.sirisakd@free.fr>
2021-05-03 17:47:00 +0200

    Init Container Registry

--- Azure/container_registry/container_registry.tf
+++ Azure/container_registry/container_registry.tf
@@ -1,4 +1,4 @@
-resource "azurerm_container_registry" "acr" {
+resource "azurerm_container_registry" "TF-ACR" {
   name                     = var.container_registry_name
   resource_group_name      = azurerm_resource_group.TF-RG.name
   location                 = azurerm_resource_group.TF-RG.location
