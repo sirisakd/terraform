490d3d5c2caea49081f723e12fc995ece41c20d1
vsirisakd <victor.sirisakd@free.fr>
2021-05-03 15:29:24 +0200

    Init user assigned identity

---
+++ Azure/user_assigned_identity/README.md
@@ -0,0 +1,10 @@
+# Azure Kubernetes Service
+
+## Providers
+
+- azurerm
+
+## Deployed components
+
+- Resource Group
+- User Assigned Managed Identity
---
+++ Azure/user_assigned_identity/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  type    = string
+  default = "RG-DEMO"
+}
+
+variable "user_assigned_identity_name" {
+  type    = string
+  default = "user-assigned-identity-demo"
+}
